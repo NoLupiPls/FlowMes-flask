70a0b025870883a531a46fc0c09691f31983ecb7
Ilya <ilyashirobokih@gmail.com>
2023-01-29 21:50:25 +0700

    Upd ID

--- flowmes/email.py
+++ flowmes/email.py
@@ -21,7 +21,5 @@ def send_password_reset_email(user):
     send_email('[FLOWMES] Reset Your Password',
                sender=app.config['ADMINS'][0],
                recipients=[user.email],
-               text_body=render_template('email/reset_password.txt',
-                                         user=user, token=token),
                html_body=render_template('email/reset_password.html',
                                          user=user, token=token))
