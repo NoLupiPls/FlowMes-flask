7937679bbb4edcc98148f33970c31680aa34d8c7
Ilya <ilyashirobokih@gmail.com>
2023-01-29 22:12:43 +0700

    Upd ru

--- flowmes/forms.py
+++ flowmes/forms.py
@@ -7,93 +7,93 @@ from flowmes.models import User
 
 
 class RegistrationForm(FlaskForm):
-    f_name = StringField('First Name', validators=[DataRequired(), Length(min=2, max=20)], render_kw={"placeholder": "First Name"})
-    l_name = StringField('Last Name', validators=[DataRequired(), Length(min=2, max=20)], render_kw={"placeholder": "Last Name"})
-    email = StringField('Email', validators=[DataRequired(), Email()], render_kw={"placeholder": "Email"})
-    gender = RadioField('Gender', choices=[('male', 'Male'), ('female', 'Female')])
-    dob = DateField('Date of Birth', format='%Y-%m-%d', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()], render_kw={"placeholder": "Password"})
-    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')], render_kw={"placeholder": "Confirm Password"})
-    submit = SubmitField('Sign Up')
+    f_name = StringField('Имя', validators=[DataRequired(), Length(min=2, max=20)], render_kw={"placeholder": "Имя"})
+    l_name = StringField('Фамидия', validators=[DataRequired(), Length(min=2, max=20)], render_kw={"placeholder": "Фамилия"})
+    email = StringField('Почта', validators=[DataRequired(), Email()], render_kw={"placeholder": "Почта"})
+    gender = RadioField('Пол', choices=[('мужчина', 'Мужчина'), ('девушка', 'Девушка')])
+    dob = DateField('Дата рождения', format='%Y-%m-%d', validators=[DataRequired()])
+    password = PasswordField('Пароль', validators=[DataRequired()], render_kw={"placeholder": "Пароль"})
+    confirm_password = PasswordField('Повторите пароль', validators=[DataRequired(), EqualTo('password')], render_kw={"placeholder": "Повторите пароль"})
+    submit = SubmitField('Зарегистрироваться')
 
     def validate_email(self, email):
         user = User.query.filter_by(email=email.data).first()
         if user:
-            raise ValidationError('That email is already in use, choose another email')
+            raise ValidationError('Эта почта уже используется. Пожалуйста, выберите другую.')
 
 
 class LoginForm(FlaskForm):
-    email = StringField('Email', validators=[DataRequired(), Email()], render_kw={"placeholder": "Email"})
-    password = PasswordField('Password', validators=[DataRequired()], render_kw={"placeholder": "Password"})
-    remember_me = BooleanField('Remember Me')
-    submit = SubmitField('Log In')
+    email = StringField('Почта', validators=[DataRequired(), Email()], render_kw={"placeholder": "Почта"})
+    password = PasswordField('Пароль', validators=[DataRequired()], render_kw={"placeholder": "Пароль"})
+    remember_me = BooleanField('Запомнить меня')
+    submit = SubmitField('Войти')
 
 
 class PostForm(FlaskForm):
     post = TextAreaField('Say something', validators=[
-        DataRequired(), Length(min=1, max=140)], render_kw={"placeholder": "What's on your mind?"})
+        DataRequired(), Length(min=1, max=140)], render_kw={"placeholder": "Что Вы хотите написать?"})
     post_image = FileField('Update Profile Picture', validators=[FileAllowed(['jpg', 'png'])])
-    submit = SubmitField('Submit')
+    submit = SubmitField('Опубликовать')
 
 
 class EditPostForm(FlaskForm):
     post = TextAreaField('Say something', validators=[
-        DataRequired(), Length(min=1, max=140)], render_kw={"placeholder": "What's on your mind?"})
+        DataRequired(), Length(min=1, max=140)], render_kw={"placeholder": "Что Вы хотите написать?"})
     post_image = FileField('Update Profile Picture', validators=[FileAllowed(['jpg', 'png'])])
-    submit = SubmitField('Submit')
+    submit = SubmitField('Опубликовать')
 
 
 class EditProfilePhotoForm(FlaskForm):
-    cover_image = FileField('Add Cover Photo', validators=[FileAllowed(['jpg', 'png'])])
-    profile_image = FileField('Add Profile Photo', validators=[FileAllowed(['jpg', 'png'])])
-    submit = SubmitField('Submit')
+    cover_image = FileField('Добавить ковер фото', validators=[FileAllowed(['jpg', 'png', 'jpeg'])])
+    profile_image = FileField('Добавить аватар', validators=[FileAllowed(['jpg', 'png', 'jpeg'])])
+    submit = SubmitField('Опубликовать')
 
 
 class EditStoryForm(FlaskForm):
-    story_image = FileField('Add Story', validators=[DataRequired(), FileAllowed(['jpg', 'png'])])
-    submit = SubmitField('Submit')
+    story_image = FileField('Создать сторис', validators=[DataRequired(), FileAllowed(['jpg', 'png', 'jpeg'])])
+    submit = SubmitField('Опубликовать')
 
 
 class EditProfileForm(FlaskForm):
-    f_name = StringField('First Name', validators=[DataRequired()])
-    l_name = StringField('Last Name', validators=[DataRequired()])
-    about_me = TextAreaField('About me', validators=[Length(min=0, max=140)])
-    submit = SubmitField('Submit')
+    f_name = StringField('Имя', validators=[DataRequired()])
+    l_name = StringField('Фамилия', validators=[DataRequired()])
+    about_me = TextAreaField('Описание', validators=[Length(min=0, max=140)])
+    submit = SubmitField('Сохранить')
 
 
 class EditProfileDetailsForm(FlaskForm):
-    school = StringField('School', validators=[DataRequired()])
-    hometown = StringField('Home Town', validators=[DataRequired()])
-    location = StringField('Current Location', validators=[DataRequired()])
-    relationship = RadioField('Gender', choices=[('single', 'Single'), ('married', 'Married'), ('dating', 'Dating')])
-    submit = SubmitField('Submit')
+    school = StringField('Школа', validators=[DataRequired()])
+    hometown = StringField('Город', validators=[DataRequired()])
+    location = StringField('Проживаете в', validators=[DataRequired()])
+    relationship = RadioField('Gender', choices=[('одинок', 'Одинок'), ('женат', 'Женат'), ('в отношениях', 'В отношениях')])
+    submit = SubmitField('Готово')
 
 
 class EmptyForm(FlaskForm):
-    submit = SubmitField('Submit')
+    submit = SubmitField('Готово')
 
 
 class CommentForm(FlaskForm):
     comment = TextAreaField('comment', validators=[
-        DataRequired(), Length(min=1, max=140)], render_kw={"placeholder": "Write a Comment"})
-    submit = SubmitField('Submit')
+        DataRequired(), Length(min=1, max=140)], render_kw={"placeholder": "Напишите комментарий"})
+    submit = SubmitField('Опубликовать')
 
 
 class LikeForm(FlaskForm):
     like = TextAreaField('comment', validators=[
-        DataRequired(), Length(min=1, max=140)], render_kw={"placeholder": "Write a Comment"})
-    submit = SubmitField('Submit')
+        DataRequired(), Length(min=1, max=140)], render_kw={"placeholder": "Напишите комментарий"})
+    submit = SubmitField('Опубликовать')
 
 
 class MessageForm(FlaskForm):
-    message = TextAreaField('Message', validators=[DataRequired(), Length(min=0, max=140)], render_kw={"placeholder": "Type your message"})
-    image = FileField('Add Photo', validators=[FileAllowed(['jpg', 'png'])])
-    submit = SubmitField('Submit')
+    message = TextAreaField('Message', validators=[DataRequired(), Length(min=0, max=140)], render_kw={"placeholder": "Введите сообщение"})
+    image = FileField('Add Photo', validators=[FileAllowed(['jpg', 'png', 'jpeg'])])
+    submit = SubmitField('Опубликовать')
 
 
 class RecipientList(FlaskForm):
-    recipient = SelectField('Select the recipient', validators=[DataRequired()])
-    submit = SubmitField('Submit')
+    recipient = SelectField('Выберите получателя', validators=[DataRequired()])
+    submit = SubmitField('Готово')
 
 
 class ResetPasswordRequestForm(FlaskForm):
